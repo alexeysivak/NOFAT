6611559f140a6eaf42656fb6c46c670b2952f501
AlSivak <olexiysyvak@gmail.com>
2021-03-21 12:01:45 +0200

    feat: changed main.scss, common.scss, homePage.js, added pictures;

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,15 +1,50 @@
+/**
+ * home page
+ */
+
 const languageSwitcher = document.getElementById('languageSwitcher');
 const burgerButton = document.getElementById('burgerButton');
 const formContainer = document.getElementById('formContainer');
+const homeBaner = document.getElementById('homeBaner');
 
-burgerButton.addEventListener('change', setLenguageSwitcher);
+burgerButton.addEventListener('change', toggleElementsVisibility);
 
-function setLenguageSwitcher() {
+function toggleElementsVisibility() {
 	if (burgerButton.checked) {
 		languageSwitcher.classList.add('hidden');
 		formContainer.classList.add('hidden');
-	} else {
+	}
+	if (window.screen.width <= 768 && burgerButton.checked) {
+		languageSwitcher.classList.add('hidden');
+		formContainer.classList.add('hidden');
+		homeBaner.classList.add('hidden');
+	}
+	if (!burgerButton.checked) {
 		languageSwitcher.classList.remove('hidden');
 		formContainer.classList.remove('hidden');
+		homeBaner.classList.remove('hidden');
 	}
 }
+
+/**
+ * home page end
+ */
+
+/**
+ * trainings page
+ */
+const trainingContainer = document.getElementById('trainingContainer');
+trainingContainer.addEventListener('click', showAdditionalInfo);
+
+function showAdditionalInfo(e) {
+	const condition = e.target.classList.contains('aditional-info__button');
+	console.log(e.target);
+	if (condition) {
+		e.target.closest('.training').classList.add('visible');
+		e.target.classList.add('hidden');
+	}
+}
+
+/**
+ * trainings page end
+ */
